ff2f60e6be42ba275f8a9898b184ad58e17b1a12
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-06-10 18:58:33 +0300

    add deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue3-store-app-test/'
+    : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
